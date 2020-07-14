a25c68a005186f7cf6aefc5ec89ac2c13ed6f8a1
guillaumequillet <bgachats@yahoo.fr>
2020-07-14 10:14:06 +0200

    switched to classic RE gameplay : no movement while aiming

--- lib/hero.rb
+++ lib/hero.rb
@@ -51,28 +51,6 @@ class Hero < Character
   def update(ennemies)
     super()
 
-    # running ?
-    set_running(Gosu::button_down?(@@keys[:run]))
-
-    # movement
-    if [:up, :right].all? {|key| Gosu::button_down?(@@keys[key])}
-      move(:north_east)
-    elsif [:up, :left].all? {|key| Gosu::button_down?(@@keys[key])}
-      move(:north_west)
-    elsif [:down, :left].all? {|key| Gosu::button_down?(@@keys[key])}
-      move(:south_west)
-    elsif [:down, :right].all? {|key| Gosu::button_down?(@@keys[key])}
-      move(:south_east)
-    elsif Gosu::button_down?(@@keys[:up])
-      move(:north) 
-    elsif Gosu::button_down?(@@keys[:down])
-      move(:south) 
-    elsif Gosu::button_down?(@@keys[:left])
-      move(:west) 
-    elsif Gosu::button_down?(@@keys[:right])
-      move(:east) 
-    end
-
     # if the player triggers aiming and some ennemy is present
     if Gosu::button_down?(@@keys[:aim]) && !ennemies.empty?
       # we set the targeting action to true
@@ -93,6 +71,30 @@ class Hero < Character
       @target = nil
       @targeting = false
     end
+
+    unless @targeting
+      # running ?
+      set_running(Gosu::button_down?(@@keys[:run]))
+
+      # movement
+      if [:up, :right].all? {|key| Gosu::button_down?(@@keys[key])}
+        move(:north_east)
+      elsif [:up, :left].all? {|key| Gosu::button_down?(@@keys[key])}
+        move(:north_west)
+      elsif [:down, :left].all? {|key| Gosu::button_down?(@@keys[key])}
+        move(:south_west)
+      elsif [:down, :right].all? {|key| Gosu::button_down?(@@keys[key])}
+        move(:south_east)
+      elsif Gosu::button_down?(@@keys[:up])
+        move(:north) 
+      elsif Gosu::button_down?(@@keys[:down])
+        move(:south) 
+      elsif Gosu::button_down?(@@keys[:left])
+        move(:west) 
+      elsif Gosu::button_down?(@@keys[:right])
+        move(:east) 
+      end
+    end
   end
 
   def draw
@@ -103,23 +105,18 @@ class Hero < Character
       angle = Gosu::angle(@position.x, @position.y, @target.position.x, @target.position.y)
 
       if angle >= 45 && angle < 135
-        faced_direction = :east
+        @direction = :east
       elsif angle >= 135 && angle < 225
-        faced_direction = :south
+        @direction = :south
       elsif angle >= 225 && angle < 315
-        faced_direction = :west
+        @direction = :west
       elsif angle >= 315 || angle < 45
-        faced_direction = :north
+        @direction = :north
       end
-
-      current_frame_index = @@directions[faced_direction] * @@frame_count + @@frame_loop_order[@frame] 
-      sprites = @aiming_sprites
-    else
-      # otherwise, we'll set the direction to the walking direction
-      current_frame_index = @@directions[@direction] * @@frame_count + @@frame_loop_order[@frame]    
-      sprites = @frames
     end
-
+    
+    current_frame_index = @@directions[@direction] * @@frame_count + @@frame_loop_order[@frame] 
+    sprites = @targeting ? @aiming_sprites : @frames   
     sprites[current_frame_index].draw_rot(@position.x, @position.y, @position.z + @position.y, 0, 0.5, 1, scale, scale)
 
     if @targeting
@@ -127,8 +124,8 @@ class Hero < Character
       @font.draw_text('targeting | angle : ' + angle.round(0).to_s, @position.x, @position.y + @font.height, 1)
       unless @target.nil?
         src = Position.new(@position.x, @position.y - 48, @position.z)
-        src.x += 36 if faced_direction == :east
-        src.x -= 36 if faced_direction == :west
+        src.x += 36 if @direction == :east
+        src.x -= 36 if @direction == :west
         Gosu::draw_line(src.x, src.y, Gosu::Color::RED, @target.position.x, @target.position.y - 48, Gosu::Color::RED)
       end
     end
